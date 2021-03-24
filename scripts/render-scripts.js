a64609f104f8c933e125bfcec855313ffc251d94
Allan Vikiru <vikiruallan12@gmail.com>
2021-03-24 09:58:01 +0300

    edit: published works link previews

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -5,21 +5,30 @@ const path = require('path');
 const sh = require('shelljs');
 
 module.exports = function renderScripts() {
-    const sourcePath = path.resolve(path.dirname(__filename), '../src/js/scripts.js');
-    const destPath = path.resolve(path.dirname(__filename), '../dist/js/scripts.js');
-    
+    const scriptsSrcPath = path.resolve(path.dirname(__filename), '../src/js/scripts.js');
+    const scriptsDestPath = path.resolve(path.dirname(__filename), '../dist/js/scripts.js');
+
+    const previewsSrcPath = path.resolve(path.dirname(__filename), '../src/js/previews.js');
+    const previewsDestPath = path.resolve(path.dirname(__filename), '../dist/js/previews.js');
+
+    const formsSrcPath = path.resolve(path.dirname(__filename), '../src/js/forms.js');
+    const formsDestPath = path.resolve(path.dirname(__filename), '../dist/js/forms.js');
+
     const copyright = `/*!
     * Start Bootstrap - ${packageJSON.title} v${packageJSON.version} (${packageJSON.homepage})
     * Copyright 2013-${new Date().getFullYear()} ${packageJSON.author}
     * Licensed under ${packageJSON.license} (https://github.com/StartBootstrap/${packageJSON.name}/blob/master/LICENSE)
     */
     `
-    const scriptsJS = fs.readFileSync(sourcePath);
-    const destPathDirname = path.dirname(destPath);
-    
+    const scriptsJS = fs.readFileSync(scriptsSrcPath);
+    const destPathDirname = path.dirname(scriptsDestPath);
+
     if (!sh.test('-e', destPathDirname)) {
         sh.mkdir('-p', destPathDirname);
     }
-    
-    fs.writeFileSync(destPath, copyright + scriptsJS);
+
+    fs.writeFileSync(scriptsDestPath, copyright + scriptsJS);
+    sh.cp('-R', previewsSrcPath, previewsDestPath);
+    sh.cp('-R', formsSrcPath, formsDestPath);
+
 };
